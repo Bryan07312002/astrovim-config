f840593d4ff576b64ca4bffe50f67cf5c3f496a5
bryan-becker-isi <bryan.becker@senairs.org.br>
2024-03-12 13:37:44 -0300

    add mini anitmation for cursor animation

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,21 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- Packs
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.docker" },
+  -- Theme
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  -- LSP
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+  -- Markdown
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- Motion
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- Other
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
 }
